abc4b0033fd62f9349d1604c46d82ab7a1698d1f
MarcMarimon <crammarimon@hotmail.com>
2024-02-24 15:22:31 +0100

    Dinamic Project done

---
+++ javascript/actualProject.js
@@ -0,0 +1,63 @@
+function loadActualProject(projectId) {
+    fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+        .then(response => response.json())
+        .then(projects => {
+            const project = projects.find(project => project.uuid === projectId);
+            if (project) {
+                const projectNameElement = document.querySelector('.project-name');
+                const projectDescriptionElement = document.querySelector('.project-description');
+                const completedOnElement = document.querySelector('.project-completed-on > date');
+                const projectImageElement = document.querySelectorAll('.project-img');
+                const projectContentElement = document.querySelector('.project-content');
+
+                projectNameElement.textContent = project.name;
+                projectDescriptionElement.textContent = project.description;
+                completedOnElement.textContent = project.completed_on;
+                projectImageElement.forEach((element) => {
+                    element.src = project.image;    
+                })
+                projectContentElement.innerHTML = project.content;
+            } else {
+                console.log('Project not found');
+            }
+
+        })
+        .catch(error => console.error('Error charging projects by ID: ', error));
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const projectId = urlParams.get('id');
+    loadActualProject(projectId);
+});
+
+function loadOtherProjects() {
+    fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+        .then(response => response.json())
+        .then(data => {
+            const actualProject = document.querySelector('.project-name').textContent;
+            const otherProjects = data.filter(project => project.name !== actualProject);
+            const projectContainers = document.querySelectorAll('.project-article');
+            for (let i = otherProjects.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [otherProjects[i], otherProjects[j]] = [otherProjects[j], otherProjects[i]];
+            }
+
+            for (let i = 0; i < projectContainers.length; i++) {
+                const projectContainer = projectContainers[i]
+                const projectImageElement = projectContainer.querySelector('img');
+                const projectNameElement = projectContainer.querySelector('h4');
+                const projectDescriptionElement = projectContainer.querySelector('p');
+                const projectLearnMoreLink = projectContainer.querySelector('a');
+
+                projectImageElement.src = otherProjects[i].image;
+                projectNameElement.textContent = otherProjects[i].name;
+                projectDescriptionElement.textContent = otherProjects[i].description;
+                projectLearnMoreLink.href = `/project.html?id=${otherProjects[i].uuid}`
+            }
+        })
+        .catch(error => console.error('Error charging other projects: ', error));
+}
+
+document.addEventListener("DOMContentLoaded", loadOtherProjects);
---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+    .then(response => response.json())
+    .then(data => {
+        const projectContainers = document.querySelectorAll('.project-article');
+        const randomProjects = getRandomProjects(data, 3);
+
+        randomProjects.forEach((project, index) => {
+            const projectContainer = projectContainers[index];
+            const projectNameElement = projectContainer.querySelector('h4');
+            const projectDescriptionElement = projectContainer.querySelector('p');
+            const projectImageElement = projectContainer.querySelector('img');
+            const projectLearnMoreLink = projectContainer.querySelector('a');
+
+            projectNameElement.textContent = project.name;
+            projectDescriptionElement.textContent = project.description;
+            projectImageElement.src = project.image;
+
+            projectLearnMoreLink.href = `/project.html?id=${project.uuid}`;
+        });
+    })
+    .catch(error => console.error('Error charging projects:', error));
+});
+
+function getRandomProjects(projects, num) {
+
+    const shuffledProjects = projects.slice();
+
+    for (let i = shuffledProjects.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledProjects[i], shuffledProjects[j]] = [shuffledProjects[j], shuffledProjects[i]];
+    }
+
+    return shuffledProjects.slice(0, num);
+}
