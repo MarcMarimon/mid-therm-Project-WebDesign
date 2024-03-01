6cfc8c57b832ed01f3ebb49a74c11bd3f904ae54
MarcMarimon <crammarimon@hotmail.com>
2024-03-01 21:17:04 +0100

    hamburger menu added to all pages

--- javascript/hamburgerMenu.js
+++ javascript/hamburgerMenu.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.querySelector('.hamburger-menu');
     const navLinkList = document.querySelector('.nav-link-list');
 
-    menuButton.addEventListener('click', function() {
+    menuButton.addEventListener('click', function () {
         navLinkList.classList.toggle('show');
     });
 });
